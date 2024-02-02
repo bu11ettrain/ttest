51d524d14208c68bda34ee2ae13440abf714006c
bu11ettrain <30414204+bu11ettrain@users.noreply.github.com>
2024-02-02 17:17:53 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,20 +12,20 @@ gpu_stats=$(< $GPU_STATS_JSON)
 	done
  screen -ls
  
- #Update
- cd ~/JettonGramGpuMiner
- #git pull
+#  #Update
+#  cd ~/JettonGramGpuMiner
+#  #git pull
  
-#Start miner
-npm install
-gpu_stats=$(< $GPU_STATS_JSON)
-	readarray -t gpu_stats < <( jq --slurp -r -c '.[] | .busids, .brand, .temp, .fan | join(" ")' $GPU_STATS_JSON 2>/dev/null)
-	busids=(${gpu_stats[0]})
-#	gpu_count=${#busids[@]}
-	gpu_count=`nvtool --gpucount | grep "GPU count:" | awk '{gsub(/[^0-9]/,""); print}'`
+# #Start miner
+# npm install
+# gpu_stats=$(< $GPU_STATS_JSON)
+# 	readarray -t gpu_stats < <( jq --slurp -r -c '.[] | .busids, .brand, .temp, .fan | join(" ")' $GPU_STATS_JSON 2>/dev/null)
+# 	busids=(${gpu_stats[0]})
+# #	gpu_count=${#busids[@]}
+# 	gpu_count=`nvtool --gpucount | grep "GPU count:" | awk '{gsub(/[^0-9]/,""); print}'`
 
-	for(( i=0; i < gpu_count; i++ )); do
-		echo Mining on GPU $i has been started
-		screen -dmS gram_$i bash -c "while true; do node ~/JettonGramGpuMiner/send_universal.js --api tonhub --givers 100 --timeout 5 --bin ~/JettonGramGpuMiner/pow-miner-cuda --gpu $i; sleep 1; done"
-	done
- screen -ls
+# 	for(( i=0; i < gpu_count; i++ )); do
+# 		echo Mining on GPU $i has been started
+# 		screen -dmS gram_$i bash -c "while true; do node ~/JettonGramGpuMiner/send_universal.js --api tonhub --givers 100 --timeout 5 --bin ~/JettonGramGpuMiner/pow-miner-cuda --gpu $i; sleep 1; done"
+# 	done
+#  screen -ls
